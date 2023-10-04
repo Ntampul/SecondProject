8dd6aadb80445262af066e0a1dec2d869bafff4d
Ntampul <ntampulamthunzi@gmail.com>
2023-10-04 14:32:23 +0200

    otp

---
+++ components/RegisterWithOTP.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+//import './Register.css';
+import { BsFillBellFill } from "react-icons/bs";
+
+const RegisterWithOTP = () => {
+  const [formData, setFormData] = useState({
+    displayName: '',
+    email: '',
+    password: '',
+    otp: '',
+    isOtpVerified: false,
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    if (formData.isOtpVerified) {
+      // Perform actual registration here
+      console.log('Registration successful!');
+    } else {
+      // Simulate OTP verification (you would replace this with actual OTP verification logic)
+      const simulatedOtp = '123456'; // Simulated OTP value
+      if (formData.otp === simulatedOtp) {
+        setFormData({ ...formData, isOtpVerified: true });
+      } else {
+        alert('OTP verification failed. Please try again.');
+      }
+    }
+  };
+
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+      <span className='logo'><BsFillBellFill color="blue" /><br></br> ChatHub</span>
+        <span className="title">SignUp</span>
+        <form onSubmit={handleSignUp}>
+         
+          {formData.isOtpVerified ? (
+            
+            <button className="button">Sign Up</button>
+          ) : (
+            <>
+              <input
+                type="text"
+                name="otp"
+                placeholder="OTP"
+                value={formData.otp}
+                onChange={handleChange}
+                required
+              />
+              <button className="buttonOTP">Verify OTP</button>
+            </>
+          )}
+        </form>
+        <p>Already have an account? Login</p>
+      </div>
+    </div>
+  );
+};
+
+export default RegisterWithOTP;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Register from '../components/Register.jsx' 
+import RegisterWithOTP from '../components/RegisterWithOTP.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Register />
-  </React.StrictMode>,
+    <RegisterWithOTP />
+  </React.StrictMode>
 )
