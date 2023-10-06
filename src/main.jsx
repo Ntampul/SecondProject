b20d1f40f848b34b1c9ddd14376cc5e7633b96d2
Ntampul <ntampulamthunzi@gmail.com>
2023-10-06 11:03:48 +0200

    absolute

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import RegisterWithOTP from './pages/RegisterWithOTP.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Register />
+      <Register />
     <RegisterWithOTP />
   </React.StrictMode>
 )
