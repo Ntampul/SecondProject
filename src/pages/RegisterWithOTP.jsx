dc729449f3d2f1380e1b7c9548e54b484661fa9d
Ntampul <ntampulamthunzi@gmail.com>
2023-11-08 14:01:12 +0200

    modal

--- src/pages/RegisterWithOTP.jsx
+++ src/pages/RegisterWithOTP.jsx
@@ -1,13 +1,11 @@
 import React, { useState } from 'react';
-import { BsFillBellFill } from "react-icons/bs";
+import { BsFillBellFill, BsArrowLeft, BsEyeSlash } from "react-icons/bs";
+
 
 const RegisterWithOTP = ({ closeModal }) => {
   const [formData, setFormData] = useState({
-    displayName: '',
     email: '',
     password: '',
-    otp: '',
-    isOtpVerified: false,
   });
 
   const handleChange = (e) => {
@@ -15,50 +13,39 @@ const RegisterWithOTP = ({ closeModal }) => {
     setFormData({ ...formData, [name]: value });
   };
 
-  const handleSignUp = (e) => {
+  const handleLogin = (e) => {
     e.preventDefault();
-    if (formData.isOtpVerified) {
-      // Perform actual registration here
-      console.log('Registration successful!');
-    } else {
-      // Simulate OTP verification (you would replace this with actual OTP verification logic)
-      const simulatedOtp = '123456'; // Simulated OTP value
-      if (formData.otp === simulatedOtp) {
-        setFormData({ ...formData, isOtpVerified: true });
-      } else {
-        alert('OTP verification failed. Please try again.');
-      }
-    }
+    // Add your login logic here using formData.email and formData.password
+    console.log('Login logic goes here');
   };
 
   return (
-    
-      <div className="formOTP modal-content absolute bg-white rounded-lg p-6 text-center shadow-md ">
-        
-        <span className='logo'><BsFillBellFill color="#7d8243" /><br></br> ChatHub</span>
-        <span className="title">Sign In with touch ID</span>
-        <form onSubmit={handleSignUp}>
-          {formData.isOtpVerified ? (
-            <button className="button">Sign In</button>
-          ) : (
-            <>
-              <input
-                type="text"
-                name="otp"
-                placeholder="OTP"
-                value={formData.otp}
-                onChange={handleChange}
-                required
-                className="w-full py-2 px-3 border rounded-md"
-              />
-              <button className="buttonOTP">Verify OTP</button>
-            </>
-          )}
-        </form>
-        <p>Already have an account? Login</p>
-       
-      </div>
-    
+    <div className="formOTP modal-content absolute bg-white rounded-lg p-6 text-center shadow-md ">
+ <span className='logo'><BsFillBellFill color="#7d8243" /><br></br> ChatHub</span>
+      <span className="title">Sign In with touch ID</span>
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          name="email"
+          placeholder="Enter Your Email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+          className="w-full py-2 px-3 border rounded-md"
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter Your Password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+          className="w-full py-2 px-3 border rounded-md"
+        />
+        <button className="button">Login</button>
+      </form>
+      <p> Don't have an account? Register</p>
+    </div>
   );
 };
 
