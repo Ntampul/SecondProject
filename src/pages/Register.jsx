e531ba314f5e1e7a8cfbf526ed1f723126ee15fa
Ntampul <ntampulamthunzi@gmail.com>
2023-11-13 11:21:08 +0200

    Close Button

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,30 +1,38 @@
 import React, { useState } from 'react';
 import './Register.css';
-import { BsFillBellFill } from "react-icons/bs";
-import { IoFingerPrintSharp } from "react-icons/io5";
-import Add from "../assets/img/avata.jpg";
-import  RegisterWithOTP from '../pages/RegisterWithOTP';
+import { BsFillBellFill } from 'react-icons/bs';
+import { IoFingerPrintSharp } from 'react-icons/io5';
+import Add from '../assets/img/avata.jpg';
+import RegisterWithOTP from '../pages/RegisterWithOTP';
 
 const Register = () => {
   const [showModal, setShowModal] = useState(false);
 
   const openModal = () => {
     setShowModal(true);
-  }
+  };
 
   const closeModal = () => {
     setShowModal(false);
-  }
+  };
 
   return (
-    <div className='page-container'>
+    <div className="page-container">
       <div className="formContainer">
         <div className="formWrapper">
-          <span className='logo'><IoFingerPrintSharp color="#7d8243" /><br></br> ChatHub</span>
-          <span className='title'>Sign in with touch ID</span>
-          <span className='tId'>Use your touch ID for faster and easier access to your account</span>
-         
-          <button className='button' onClick={openModal}>SignUp</button>
+          <span className="logo">
+            <IoFingerPrintSharp color="#7d8243" />
+            <br />
+            ChatHub
+          </span>
+          <span className="title">Sign in with touch ID</span>
+          <span className="tId">
+            Use your touch ID for faster and easier access to your account
+          </span>
+
+          <button className="button" onClick={openModal}>
+            SignUp
+          </button>
 
           <label htmlFor="file" className="avatar-label">
             <div className="avatar-container">
@@ -35,24 +43,27 @@ const Register = () => {
           </label>
 
           <p>New User? Sign Up</p>
-          <span className='logo'><BsFillBellFill color="#7d8243" /></span><br></br>
-          {showModal && (
-               <div className="modal">
-               <div class="modal-container">
-  <div class="modal">
-    <button class="close-button">×</button>
-    <RegisterWithOTP />
-   
-  </div>
-  
-</div>
+          <span className="logo">
+            <BsFillBellFill color="#7d8243" />
+          </span>
+          <br />
 
+          {showModal && (
+            <div className="modal">
+              <div className="modal-container">
+                <div className="modal">
+                  <button className="close-button" onClick={closeModal}>
+                    ×
+                  </button>
+                  <RegisterWithOTP />
+                </div>
               </div>
+            </div>
           )}
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Register;
