b25795ee885b8ca98bc52d2bc0ef910649880984
Ntampul <ntampulamthunzi@gmail.com>
2023-11-23 13:06:35 +0200

    navbar

--- Chat/components/Navbar.jsx
+++ Chat/components/Navbar.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
-      <span classname='logo'>ChatHub</span>
+    <div className="navbar">
+      <span className="logo">ChatHub</span>
+      <div className="menu">
+        <button className="hamburger">&#9776;</button>
+        <button className="search">Search</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
